76a4fcd1af84be9a92fe1ea59596e431466977cd
Яков <golovanov_yakov@list.ru>
2026-08-31 21:40:56 +0300

    Update 4_bookings.sql
    Меняем идентификацию пассажиров с name на id

--- My portfolio/Airlines/4_bookings.sql
+++ My portfolio/Airlines/4_bookings.sql
@@ -1,7 +1,19 @@
-SELECT bc.book_count,
-	   COUNT(*)
-FROM (
-	SELECT passenger_name, ROW_NUMBER() OVER(PARTITION BY passenger_name ORDER BY passenger_name) AS book_count
-	FROM tickets) AS bc
-GROUP BY bc.book_count
-ORDER BY bc.book_count
+WITH unique_user_bookings AS (
+    -- Шаг 1: Находим только уникальные бронирования для каждого пассажира по его ID
+    SELECT DISTINCT passenger_id, book_ref
+    FROM tickets
+),
+booked_sequences AS (
+    -- Шаг 2: Нумеруем бронирования по порядку для каждого уникального пользователя
+    SELECT 
+        passenger_id,
+        ROW_NUMBER() OVER(PARTITION BY passenger_id ORDER BY book_ref) AS booking_order
+    FROM unique_user_bookings
+)
+-- Шаг 3: Считаем пользователей на каждом шаге для построения воронки удержания
+SELECT 
+    booking_order AS "Порядковый номер бронирования",
+    COUNT(*) AS "Количество пользователей"
+FROM booked_sequences
+GROUP BY booking_order
+ORDER BY booking_order;
