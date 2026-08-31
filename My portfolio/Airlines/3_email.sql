dde0a0b2717fa1516818b70b50429686577f51f9
Яков <golovanov_yakov@list.ru>
2026-08-31 21:38:12 +0300

    Update 3_email.sql
    Убираем опечатки, оптимизируем код

--- My portfolio/Airlines/3_email.sql
+++ My portfolio/Airlines/3_email.sql
@@ -1,11 +1,9 @@
-WITH
-cd AS (SELECT ts.contact_data
-	  FROM flights AS fl
-	  INNER JOIN airports AS ap ON ap.airport_code=fl.arrival_airport
-	  INNER JOIN ticket_flights AS tf ON tf.flight_id=fl.flight_id
-	  INNER JOIN tickets AS ts ON ts.ticket_no=rf.ricket_no
-	  WHERE EXTRACT(MONTH FROM fl.scheduled_departure) IN (6, 7, 8)
-	  AND ap.city = 'Москва')
-SELECT js.value
-FROM cd, jsonb_each(cd.contact_data) AS js
-WHERE ls.key = 'email'
+SELECT DISTINCT 
+    ts.contact_data->>'email' AS email
+FROM flights AS fl
+INNER JOIN airports_data AS ap ON ap.airport_code = fl.arrival_airport
+INNER JOIN ticket_flights AS tf ON tf.flight_id = fl.flight_id
+INNER JOIN tickets AS ts ON ts.ticket_no = tf.ticket_no
+WHERE EXTRACT(MONTH FROM fl.scheduled_departure) IN (6, 7, 8)
+  AND ap.city->>'ru' = 'Москва'
+  AND ts.contact_data->>'email' IS NOT NULL;
