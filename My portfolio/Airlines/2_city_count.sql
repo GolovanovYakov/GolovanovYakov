a8b0399d59ba8377ed03bfa5494a27a8465dab1e
Яков <golovanov_yakov@list.ru>
2026-08-31 21:23:31 +0300

    Update 2_city_count.sql
    Исправлены ошибки

--- My portfolio/Airlines/2_city_count.sql
+++ My portfolio/Airlines/2_city_count.sql
@@ -1,6 +1,8 @@
-SELECT ap.city AS city,
-	COUNT(fl.flight_id)
-FROM flights AS dl
-INNER JOIN airports AS ap ON ap.airport_code=fl.arrival_airport
-GROUP BY city
-ORDER BY COUNT(fl.flight_id) DESC
+SELECT 
+    ap.city->>'ru' AS city_name,
+    COUNT(fl.flight_id) AS flights_count
+FROM flights AS fl
+INNER JOIN airports_data AS ap ON ap.airport_code = fl.arrival_airport
+WHERE EXTRACT(YEAR FROM fl.scheduled_departure) = 2017	
+GROUP BY ap.city->>'ru'
+ORDER BY COUNT(fl.flight_id) DESC;
